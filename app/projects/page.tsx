a92021759d6a413b5ea051ee1dd528c69a54a0d7
Endelif <92841501+idkwhyi@users.noreply.github.com>
2024-12-17 15:03:29 +0700

    Change text on project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -46,7 +46,7 @@ export default async function ProjectsPage() {
             Projects
           </h2>
           <p className="mt-4 text-zinc-400">
-            Some of the projects are from work and some are on my own time.
+            The <u>Projects</u> that I am working on
           </p>
         </div>
         <div className="w-full h-px bg-zinc-800" />
